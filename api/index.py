265854b8db7eaea76bdde0b992acf36c43ef661f
soma30121996 <85888424+soma30121996@users.noreply.github.com>
2025-11-27 13:55:35 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,10 @@
-from app import app
+from fastapi import FastAPI
 from mangum import Mangum
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "FastAPI deployed successfully on Vercel 🎉"}
 
 handler = Mangum(app)
